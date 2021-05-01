eddbac48f5481701872dce513e5aa7af25bff457
Rehan <rehans@protonmail.com>
2021-05-01 23:27:49 +0800

    Scrape on an individual item level

--- silverbullion/scrape_tester.py
+++ silverbullion/scrape_tester.py
@@ -2,9 +2,24 @@
 import json
 import requests
 import os
+import logging
 import boto3
 import datetime as dt
 from bs4 import BeautifulSoup
+
+#%%
+def get_items_urls(html_text):
+    soup = BeautifulSoup(html_text, 'html.parser')
+    list_tiers = soup.find_all(attrs={'class' : "list-tiers"})
+    items_urls = [t.find(lambda tag: tag.name =='meta' and tag.get('itemprop') =='url') for t in list_tiers]
+    items_urls = [tag.get("content") for tag in items_urls if tag]
+    return items_urls
+
+
+#%%
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
 
 dest_dir = r"/home/rehan/Documents/Python/Projects/bytenami/data"
 base_url = "https://www.silverbullion.com.sg"
@@ -17,28 +32,37 @@ product_urls = [
 ]
 
 for product in product_urls:
-    page_no = 1
+    items_urls = []
     product_type = product.split("/")[-1]
-    url_data = {}
+    result_data = ""
+    logging.info(f"Scraping {product_type}")
     data = requests.get(base_url + product)
-    url_data[page_no] = data.text
+    items_urls += get_items_urls(data.text)
     soup = BeautifulSoup(data.text, 'html.parser')
     pagination = soup.find(class_ = "pagination-container")
     if pagination:
+        logging.info(f"Mutiple pages found for {product_type}")
         pages = pagination.find(class_ = "PagedList-skipToNext")
+        counter = 1
         while pages:
-            page_no += 1
+            counter += 1
             next_page_url = base_url + pages.find("a").get("href")
+            logging.info(f"Scraping page {counter}, URL: {next_page_url}")
             data_pg = requests.get(next_page_url)
-            url_data[page_no] = data_pg.text
+            items_urls += get_items_urls(data_pg.text)
             soup = BeautifulSoup(data_pg.text, 'html.parser')
             pagination = soup.find(class_ = "pagination-container")
             pages = pagination.find(class_ = "PagedList-skipToNext")
-    for k, v in url_data.items():
-        fnam = f"silver_bullion_{product_type.lower()}_{k}_{dt.datetime.utcnow()}.html"
-        #s3.Object("bn-silverbullion", fnam).put(Body=v)
-        with open(os.path.join(dest_dir, fnam), "w") as f:
-            f.write(v)
+    items_urls = list(set(items_urls))
+    logging.info(f"Found {len(items_urls)} items under {product_type}")
+    for item in items_urls:
+        logging.info(f"Scraping {item}")
+        data_item = requests.get(item)
+        result_data += data_item.text
+    fnam = f"silver_bullion_{product_type.lower()}_{dt.datetime.utcnow()}.html"
+    #s3.Object("bn-silverbullion", fnam).put(Body=result_data)
+    with open(os.path.join(dest_dir, fnam), "w") as f:
+        f.write(result_data)
 
 #TODO
 # use <div class="list-tiers"> to get product URLS from each page
@@ -47,6 +71,11 @@ for product in product_urls:
 
 
 #%%
-fnam = f"silver_bullion_gold_{dt.datetime.now()}.html"
-s3.Object("bn-silverbullion", fnam).put(Body=data.text)
-return {"statusCode": 200, "body": len(data.text)}
+'''
+url = "https://www.silverbullion.com.sg/Shop/Buy/Silver_Bars"
+data = requests.get(url)
+soup = BeautifulSoup(data.text, 'html.parser')
+list_tiers = soup.find_all(attrs={'class' : "list-tiers"})
+items_urls = [t.find(lambda tag: tag.name =='meta' and tag.get('itemprop') =='url') for t in list_tiers]
+items_urls = [tag.get("content") for tag in items_urls if tag]
+'''
